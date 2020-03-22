a0b9b53d1390fe670fa7f860c27e31a3a53a748f
Devgen <dev@generationyct.nl>
2020-03-22 15:19:35 +0100

    Added more styling

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', ensureAuthenticated, (req, res, next) => {
+  res.render('index', {user: req.user});
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    res.redirect('/login')
+  }
+}
 
 module.exports = router;
